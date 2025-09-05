95f4873e2ac7640986c5be4dc8709fc2b4fc3729
zhanghaotian <haotian.zhang5@transsion.com>
2025-09-05 17:06:10 +0800

    fix: pnpm 10

--- packages/vue3/histoire.setup.ts
+++ packages/vue3/histoire.setup.ts
@@ -2,19 +2,6 @@ import { defineSetupVue3 } from '@histoire/plugin-vue'
 import { setupDirectives } from '@/directives'
 import { setupStore } from '@/store'
 import AppProvider from '@/common/AppProvider/index.vue'
-// export const setupVue3 = defineSetupVue3(({ app }) => {
-//   setupDirectives(app);
-//   setupStore(app);
-
-//   return {
-//     // 用 wrapper 把每个 story 包裹在 MessageProvider 中
-//     wrapper: (storyComponent: any) => {
-//       return h(NMessageProvider, null, {
-//         default: () => storyComponent,
-//       })
-//     },
-//   }
-// })
 
 export const setupVue3 = defineSetupVue3(({ app,addWrapper }) => {
   setupDirectives(app);
