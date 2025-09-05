e4c6b9eed0dd77734e578cbbdb9ceeaa81827c3e
zhanghaotian <haotian.zhang5@transsion.com>
2025-09-05 17:35:33 +0800

    fix: alias

--- packages/vue3/vite.config.base.ts
+++ packages/vue3/vite.config.base.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'date-fns-tz/dist/esm': 'date-fns-tz/dist/esm/index.js',
     },
   },
   plugins: [
